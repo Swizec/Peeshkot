5f0cba18214054277feb7b04b6f3172fa6e21dbc
Swizec Teller <swizec@swizec.com>
2013-06-17 00:59:50 +0200

    can't get page action javascript to work

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,7 @@ chrome.extension.sendMessage({}, function(response) {
     var readyStateCheckInterval = setInterval(function() {
         if (document.readyState === "complete") {
             clearInterval(readyStateCheckInterval);
-            findNotices(handleNotices);
+            //findNotices(handleNotices);
         }
     }, 10);
 });
