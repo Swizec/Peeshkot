48d990e00fc05a89030362e73336245525b10fb7
Swizec Teller <swizec@swizec.com>
2013-06-16 16:56:50 +0200

    clear some defaults

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,13 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
+    var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
+	    clearInterval(readyStateCheckInterval);
+            
+	    // ----------------------------------------------------------
+	    // This part of the script triggers when page is done loading
+	    console.log("Hello. This message was sent from scripts/inject.js");
+	    // ----------------------------------------------------------
+            alert("Hello they are!");
 	}
-	}, 10);
+    }, 10);
 });
