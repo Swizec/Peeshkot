40f2ecae3963621cbf400688d07c29dd59a55faa
Ivan Tatic <ivan@simplified.me>
2013-06-25 20:59:49 +0200

    Adds val202, radiostudent, mojazaposlitev.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,9 +7,9 @@
             }
             }, 10);
     });
-    var holderSelectors = ['#allow_cookies_container', '#cookieGuardMsg', '.cc-cookies', '.cookies-header', '#huk_cookie_prefernce_panel', '#cookies', '#cookie-law-wrapper', '#avcn_wrapperInner', '#cookiesWarning', '#cookiewarning', '#cookieok_bar', '#cccwr', '#epbar', '#icanhascookie', '#cc-notification', '#boxZgodaNaCookie', '#cookieMonsterWrapper', '#cookiePolicyPopup', '#fp_cookieMessageContainer', '#WP-cookie-info', '.cookieTooltip', 'div[class^="cookie-alert"]', 'div[id="ciasteczka"]', 'div[id="cookie-info"]', 'div[id="cookieInfoMsgWrapper"]', 'div[id="cookiepolicydiv"]', 'div[class^="pea_cook_wrapper"]', 'div[id="cookies-info"]', '#cookielaw', '#cookiewarn', '#cookiesMessageBanner', '#bbccookies', '.notification.cookie', '#cookiePolicy', '#fp_cookieMessageContainer', '#cookieBar', '.divCookieWarning', '#divCookie', '#cookie-policy-container', '.footer-cookies-policy', '#cookieNotification', '#cookie-bar', '#noScriptCookies', '#block-cookie-info', '.cookie-message', '#cookieMessageWrapper', '#cookie-bar', '#bauerCookiePolicy', '#cookie-banner', '#cookielaw', '#fp_cookieMessageContainer', '#cookieBar', '#cookie-compliance', '#id_cookieconsent', '#cookiesInitialDialog', '#cookiesdirective', '.cookieBanner', '.cookieStandard', '#bauerCookiePolicy', '.optanon-alert-box-wrapper', '.identity-noticebar', '.optanon-alert-box-wrapper', '#cookies-alert', '#cow_overlay_inside', '.cookiesNotice'].join(', '),
-        buttonsSelectors = ['#epb-ok', '.cc-cookie-accept', '#authoriseCookies', '.choice-agree', '#EU_OPIN_CANCEL', '#floaterAgree', '#cookieok_check', '.iAgree', '.cookieAgree', '#cookie-compliance-submit', '.cc-link', '#cctoggle', '#ichok', '.hide-bar', '.optanon-alert-box-close', '.cookies-yes', '.cc-approve-button-thissite', '.continue', 'img[src*="cookies_button"]', '#avcn_agreeButton', '#cookieTermsagree', '#btncookie_ok', '.btncookie_ok'].join(', '),
-        buttonText = ['Ok', 'V redu', 'Accept', 'Razumem', 'I agree', 'Dovoli piškotke', 'Sem seznanjen', 'Shrani', 'Strinjam se', 'Se strinjam', 'Da', 'Sprejemam', 'Nadaljuj', 'Sprejmi', 'Dovolim piškotke', 'Nadaljuj z branjem', 'Shrani nastavitve'],
+    var holderSelectors = ['#allow_cookies_container', '#cookieGuardMsg', '.cc-cookies', '.cookies-header', '#huk_cookie_prefernce_panel', '#cookies', '#cookie-law-wrapper', '#avcn_wrapperInner', '#cookiesWarning', '#cookiewarning', '#cookieok_bar', '#cccwr', '#epbar', '#icanhascookie', '#cc-notification', '#boxZgodaNaCookie', '#cookieMonsterWrapper', '#cookiePolicyPopup', '#fp_cookieMessageContainer', '#WP-cookie-info', '.cookieTooltip', 'div[class^="cookie-alert"]', 'div[id="ciasteczka"]', 'div[id="cookie-info"]', 'div[id="cookieInfoMsgWrapper"]', 'div[id="cookiepolicydiv"]', 'div[class^="pea_cook_wrapper"]', 'div[id="cookies-info"]', '#cookielaw', '#cookiewarn', '#cookiesMessageBanner', '#bbccookies', '.notification.cookie', '#cookiePolicy', '#fp_cookieMessageContainer', '#cookieBar', '.divCookieWarning', '#divCookie', '#cookie-policy-container', '.footer-cookies-policy', '#cookieNotification', '#cookie-bar', '#noScriptCookies', '#block-cookie-info', '.cookie-message', '#cookieMessageWrapper', '#cookie-bar', '#bauerCookiePolicy', '#cookie-banner', '#cookielaw', '#fp_cookieMessageContainer', '#cookieBar', '#cookie-compliance', '#id_cookieconsent', '#cookiesInitialDialog', '#cookiesdirective', '.cookieBanner', '.cookieStandard', '#bauerCookiePolicy', '.optanon-alert-box-wrapper', '.identity-noticebar', '.optanon-alert-box-wrapper', '#cookies-alert', '#cow_overlay_inside', '.cookiesNotice', '#cookiePolicyCW'].join(', '),
+        buttonsSelectors = ['#epb-ok', '.cc-cookie-accept', '#authoriseCookies', '.choice-agree', '#EU_OPIN_CANCEL', '#floaterAgree', '#cookieok_check', '.iAgree', '.cookieAgree', '#cookie-compliance-submit', '.cc-link', '#cctoggle', '#ichok', '.hide-bar', '.optanon-alert-box-close', '.cookies-yes', '.cc-approve-button-thissite', '.continue', 'img[src*="cookies_button"]', '#avcn_agreeButton', '#cookieTermsagree', '#btncookie_ok', '.btncookie_ok', '.agree-button', '.cookieAccept'].join(', '),
+        buttonText = ['Ok', 'V redu', 'Accept', 'Razumem', 'I agree', 'Dovoli piškotke', 'Sem seznanjen', 'Shrani', 'Strinjam se', 'Se strinjam', 'Da', 'Sprejemam', 'Nadaljuj', 'Yes, I agree', 'Sprejmi', 'Dovolim piškotke', 'Nadaljuj z branjem', 'Shrani nastavitve'],
         keywords = ['cookie', 'piskot', 'piškot' ],
         buttonNodes = ['a', 'button', 'input[type="submit"]', 'input[type="button"]'],
         domainSpecific = {
@@ -17,7 +17,9 @@
         ,   'bankain.si': {node: '#LoggingCookie', button: '.button'}
         ,   'bolha.com': {node: '#cookiesWarning', button: '.iAgree'}
         ,   'nepremicnine.net': {node: '#cookieWarn', button: '#cookieTermsagree'}
+        ,   'mojazaposlitev.si': {node: '#cookiePolicyCW', button: '.cookieAccept'}
         ,   'delo.si': {node: '#cboxWrapper', button: '#continue'}
+        ,   'val202.si': {node: '#cookies', button: 'img[src*="cookies_button"]'}
         ,   'dnevnik.si': {node: '#cc-notification', button: '.cc-set-all'}
         ,   'izklop.com': {node: '#cc-notification', button: '#cc-approve-button-thissite'}
         ,   'had.si': {node: '#cc-notification', button: '#cc-approve-button-thissite'}
@@ -40,6 +42,7 @@
         ,   'kulinarika.net': {node: '.cc-cookies', button: '.cc-cookie-accept'}
         ,   'bizi.si': {node: '.cc-cookies', button: '.cc-cookie-accept'}
         ,   'oskarveliki.si': {node: '.cc-cookies', button: '.cc-cookie-accept'}
+        ,   'radiostudent.si': {node: '#sliding-popup', button: '.agree-button'}
         ,   'najdi.si': {node: '.cc-cookies', button: '.cc-cookie-accept'}
         ,   'shrani.najdi.si': {node: '.cc-cookies', button: '.cc-cookie-accept'}
         ,   'zurnal24.si': {node: '#cookie_law_notice_container', button: '.agree_to_cookies'}
